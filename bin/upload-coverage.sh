64e8a8efaa22307e7eff31759cf3b82850236585
peter279k <peter279k@gmail.com>
2020-11-06 00:52:21 +0800

    Fix cc-test-reporter format for clover

--- bin/upload-coverage.sh
+++ bin/upload-coverage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 if [ "$TRAVIS_PHP_VERSION" != "hhvm" ]; then
-    ./cc-test-reporter format-coverage -t simplecov -o codeclimate.json
+    ./cc-test-reporter format-coverage -t clover -o codeclimate.json
     curl -X POST -d @codeclimate.json -H "Content-Type: application/json" -H "User-Agent: Code Climate (PHP Test Reporter v1.0.1-dev)" https://codeclimate.com/test_reports
 fi;
